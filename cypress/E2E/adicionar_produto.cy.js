82ccad8fdec68f1b2b0f01cb35c0e535f8d1bc88
Layanne Fagundes <52285246+Layanne-Fagundes@users.noreply.github.com>
2024-07-18 21:14:56 -0300

    test: mudança do nome BackPack

--- cypress/E2E/adicionar_produto.cy.js
+++ cypress/E2E/adicionar_produto.cy.js
@@ -1,18 +1,17 @@
 describe('adicionar produto', () => {
 
     beforeEach(() => {
-		cy.visit('https://www.saucedemo.com/v1/');
     cy.login('standard_user', 'secret_sauce');
 	});
   it('Deve adicionar mais de um item ao carrinho', () => {
     // Adiciona o primeiro item ao carrinho
-    cy.addItem('Sauce Labs BackPack');
+    cy.addItem('Sauce Labs Backpack');
   
     // Adiciona o segundo item ao carrinho
     cy.addItem('Sauce Labs Bike Light');
     
     // Verificar se os itens foram adicionados ao carrinho
-    cy.get('.shopping_cart_bagde').should('have.text', 2);
+    cy.get('.shopping_cart_badge').should('have.text', '2');
   });
 });
 
